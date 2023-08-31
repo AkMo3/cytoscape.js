2c4f3a99487a93b6229ee92b89a43a2c2be01ff6
Akash Mondal <akmo3901@gmail.com>
2023-08-31 12:51:35 +0530

    Fix: test prev version

--- .github/workflows/scripts/new-patch-version.sh
+++ .github/workflows/scripts/new-patch-version.sh
@@ -4,21 +4,25 @@ PREV_VERSION=$(jq -r '.version' package.json)
 echo "Prev Patch Version $PREV_VERSION"
 
 # Split the version number into major, minor, and patch components
-IFS='.' read -ra VERSION_ARRAY <<< "$PREV_VERSION"
+IFS='.' read -a VERSION_ARRAY <<< "$PREV_VERSION"
 echo "SPLITTING COMPLETED"
-PATCH_VERSION="${VERSION_ARRAY[2]}"
-echo "CURRENT PATCH VERSION" $PATCH_VERSION
 
-# Remove leading zeros (if any) from PATCH_VERSION
-PATCH_VERSION="${PATCH_VERSION#"${PATCH_VERSION%%[1-9]*}"}"
+major=${version_parts[0]}
+minor=${version_parts[1]}
+patch=${version_parts[2]}
+echo "CURRENT PATCH VERSION" $patch
 
-# Increment patch for the new backport branch
-PATCH_VERSION=$((10#${PATCH_VERSION} + 1))
-PATCH_VERSION="${PATCH_VERSION}"
+patch=$((patch + 1))
+echo "UPDATED PATCH VERSION" $patch
 
-echo "UPDATED PATCH VERSION" $PATCH_VERSION
+VERSION="$major.$minor.$patch"
 
-VERSION="${VERSION_ARRAY[0]}.${VERSION_ARRAY[1]}.${PATCH_VERSION}"
+# Split the version number into major, minor, and patch components
+IFS='.' read -a VERSION_ARRAY_2 <<< "$VERSION"
+if [[ ${#VERSION_ARRAY_2[@]} -lt 3 ]]; then
+    echo "Error: Invalid new version format"
+    exit 1
+fi
 
 if [$BRANCH != "refs/heads/master"]; then
     BRANCH="${VERSION_ARRAY[0]}.${VERSION_ARRAY[1]}.x"
